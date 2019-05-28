6f02b27bbc7832794aaaa2e44299506bef1dc87a
paulollerton <paul.ollerton@gmail.com>
2019-05-27 18:00:45 -0700

    adds authorization to users

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::UsersController < ApplicationController
+
+  before_action :authenticate_admin, except: [:index, :show]
+
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    
+    if user.save
+      render json: {message: "User cretaed successfully"}, status: :cretaed
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,20 @@ Rails.application.routes.draw do
   namespace :api do
 
     get "/movies" => "movies#index"
-
     get "/movie/:id" => "movies#show"
-
     post "/movie" => "movies#create"
-
     patch "/movie/:id" => "movies#update"
-
     delete "/movie/:id" => "movies#destroy"
 
     get "/actors" => "actors#index"
-
     get "actor/:id" => "actors#show"
-
     post "/actor" => "actors#create"
-
     patch "actor/:id" => "actors#update"
-
     delete "actor/:id" => "actors#destroy"
+
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
 
   end
 
